cb091d7fbae39ec2571e21c1e68d62eff168c030
gjularic <gvozdan.jularic@gmail.com>
2021-12-29 17:56:52 +0000

    Add main variables

--- run.py
+++ run.py
@@ -95,6 +95,7 @@ HANGMAN = [
 ]
 
 #Main game
+
 print("Hangman game")
 print(HANGMAN[7])
 name = input("Enter your name: ")
@@ -102,7 +103,9 @@ print("Welcome", name, "!")
 print("Try to guess the word before the stickman gets hanged, you have 7 guesses available!")
 
 
-#Function to get the random word and change to uppercase
-def random_word():
-        word = random.choice(word_list)
-        return word.upper()
+#Main Variables
+word = random.choice(word_list).upper()
+current_guess = "-" * len(word)
+max_guess = 7
+wrong_guess = 0
+guessed_letters = []
